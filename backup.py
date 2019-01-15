bd8f73704cf365b38c348b44a7dd31802d8127a5
Paul Greidanus <paul@moodle.com>
2019-01-15 16:37:21 -0700

    Adding CLI options for course ID

--- backup.py
+++ backup.py
@@ -3,22 +3,30 @@ import configparser
 import string
 import random
 import mdllib
+import argparse
 
-# parse the config
- # username
- # password
- # site URL
- # course id to backup
+parser = argparse.ArgumentParser(description='Backs up a course from a Moodle instance')
+parser.add_argument('--courseid', metavar='courseid', type=str,
+                    help='The ID of the course you wish to back up',
+                    default=0)
+parser.add_argument('--config', dest='configfile',
+                    help='Name of the config file to read',
+                    default='config.cfg')
 
-
+args = parser.parse_args()
+configfile = args.configfile
+courseid = args.courseid
 
 config = configparser.ConfigParser()
-config.read('config.cfg')
+config.read(configfile)
 
 username = config['DEFAULT']['username']
 password = config['DEFAULT']['password']
 url = config['DEFAULT']['url']
-courseid = config['DEFAULT']['course']
+if courseid == 0:
+    courseid = config['DEFAULT']['course']
+else:
+    print("Course ID %s specified by CLI option, ignoring course id of %s from config file"%(courseid, (config['DEFAULT']['course'])))
 proxies = None
 if config['DEFAULT']['http_proxy']:
     proxies = {}
